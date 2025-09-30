4d0f3ebe52ba05aeeb90b1117becc4b1f70ab104
JaimeBonBol <jaimebb2002@gmail.com>
2025-09-30 14:51:35 +0200

    Comienzo calculadora con JavaFX

---
+++ JavaFX/Calculadora-javaFX/src/controlador/Main.java
@@ -0,0 +1,46 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMain.java to edit this template
+ */
+package controlador;
+
+import javafx.application.Application;
+import static javafx.application.Application.launch;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author jaimedam
+ */
+public class Main extends Application {
+    
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+
+            Parent root = FXMLLoader.load(getClass().getResource("/vista/vistaCalculadora.fxml"));
+
+            // Cargo el scene
+            
+            Scene scene = new Scene(root);
+            scene.getStylesheets().add(
+                getClass().getResource("/css/theme_light.css").toExternalForm()
+            );
+            
+            primaryStage.setTitle("Calculadora-JavaFX");
+            // Seteo la scene y la muestro
+            primaryStage.setScene(scene);
+            primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+}
---
+++ JavaFX/Calculadora-javaFX/src/controlador/VistaCalculadoraController.java
@@ -0,0 +1,73 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
+ */
+package controlador;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.Pane;
+
+/**
+ * FXML Controller class
+ *
+ * @author jaimedam
+ */
+public class VistaCalculadoraController implements Initializable {
+
+    @FXML
+    private Button btn7;
+    @FXML
+    private Button btn8;
+    @FXML
+    private Button btn9;
+    @FXML
+    private Button btn4;
+    @FXML
+    private Button btn5;
+    @FXML
+    private Button btn6;
+    @FXML
+    private Button btn1;
+    @FXML
+    private Button btn2;
+    @FXML
+    private Button btn3;
+    @FXML
+    private Button btn0;
+    @FXML
+    private Button btnAC;
+    @FXML
+    private Button btnDivision;
+    @FXML
+    private Button btnMultiplicacion;
+    @FXML
+    private Button btnSuma;
+    @FXML
+    private Button btnResta;
+    @FXML
+    private Button btnPorcentaje;
+    @FXML
+    private Button btnResultado;
+    @FXML
+    private Pane pantalla;
+    @FXML
+    private Label labelOperador1;
+    @FXML
+    private Label labelOperador2;
+    @FXML
+    private Label labelOperacion;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
