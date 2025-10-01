783d23f86cbf97ce53f40a7f1abd8c1aae284928
JaimeBonBol <jaimebb2002@gmail.com>
2025-10-01 10:06:12 +0200

    Desarrollo interfaz Calculadora con funcionalidad, con JavaFX

--- JavaFX/Calculadora-javaFX/src/controlador/VistaCalculadoraController.java
+++ JavaFX/Calculadora-javaFX/src/controlador/VistaCalculadoraController.java
@@ -6,11 +6,13 @@ package controlador;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.Pane;
+import modelo.Operaciones;
 
 /**
  * FXML Controller class
@@ -61,6 +63,9 @@ public class VistaCalculadoraController implements Initializable {
     private Label labelOperador2;
     @FXML
     private Label labelOperacion;
+    @FXML
+    private Label labelResultado;
+    
 
     /**
      * Initializes the controller class.
@@ -69,5 +74,227 @@ public class VistaCalculadoraController implements Initializable {
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
     }    
+
+    @FXML
+    private void clickBtn0(ActionEvent event) {
+        
+        if(labelOperacion.getText().isEmpty()){
+            labelOperador1.setText(labelOperador1.getText() + "0");
+            // Se comprueba en el ese si la pantalla resultado está´a para una vez que se ha mostrado
+            // el resultado no se pongan más números en la pantalla del operador 2
+        }else if(labelResultado.getText().isEmpty()){
+            labelOperador2.setText(labelOperador2.getText() + "0");
+        } 
+        
+    }
+
+    @FXML
+    private void clickBtn7(ActionEvent event) {
+        
+        if(labelOperacion.getText().isEmpty()){
+            labelOperador1.setText(labelOperador1.getText() + "7");
+            // Se comprueba en el ese si la pantalla resultado está´a para una vez que se ha mostrado
+            // el resultado no se pongan más números en la pantalla del operador 2
+        }else if(labelResultado.getText().isEmpty()){
+            labelOperador2.setText(labelOperador2.getText() + "7");
+        }
+        
+    }
+
+    @FXML
+    private void clickBtn8(ActionEvent event) {
+        
+        if(labelOperacion.getText().isEmpty()){
+            labelOperador1.setText(labelOperador1.getText() + "8");
+            // Se comprueba en el ese si la pantalla resultado está´a para una vez que se ha mostrado
+            // el resultado no se pongan más números en la pantalla del operador 2
+        }else if(labelResultado.getText().isEmpty()){
+            labelOperador2.setText(labelOperador2.getText() + "8");
+        }
+        
+    }
+
+    @FXML
+    private void clickBtn9(ActionEvent event) {
+        
+        if(labelOperacion.getText().isEmpty()){
+            labelOperador1.setText(labelOperador1.getText() + "9");
+            // Se comprueba en el ese si la pantalla resultado está´a para una vez que se ha mostrado
+            // el resultado no se pongan más números en la pantalla del operador 2
+        }else if(labelResultado.getText().isEmpty()){
+            labelOperador2.setText(labelOperador2.getText() + "9");
+        }
+        
+    }
+
+    @FXML
+    private void clickBtn4(ActionEvent event) {
+        
+        if(labelOperacion.getText().isEmpty()){
+            labelOperador1.setText(labelOperador1.getText() + "4");
+            // Se comprueba en el ese si la pantalla resultado está´a para una vez que se ha mostrado
+            // el resultado no se pongan más números en la pantalla del operador 2
+        }else if(labelResultado.getText().isEmpty()){
+            labelOperador2.setText(labelOperador2.getText() + "4");
+        }
+        
+    }
+
+    @FXML
+    private void clickBtn5(ActionEvent event) {
+        
+        if(labelOperacion.getText().isEmpty()){
+            labelOperador1.setText(labelOperador1.getText() + "5");
+            // Se comprueba en el ese si la pantalla resultado está´a para una vez que se ha mostrado
+            // el resultado no se pongan más números en la pantalla del operador 2
+        }else if(labelResultado.getText().isEmpty()){
+            labelOperador2.setText(labelOperador2.getText() + "5");
+        }
+        
+    }
+
+    @FXML
+    private void clickBtn6(ActionEvent event) {
+        
+        if(labelOperacion.getText().isEmpty()){
+            labelOperador1.setText(labelOperador1.getText() + "6");
+            // Se comprueba en el ese si la pantalla resultado está´a para una vez que se ha mostrado
+            // el resultado no se pongan más números en la pantalla del operador 2
+        }else if(labelResultado.getText().isEmpty()){
+            labelOperador2.setText(labelOperador2.getText() + "6");
+        }
+        
+    }
+
+    @FXML
+    private void clickBtn1(ActionEvent event) {
+        
+        if(labelOperacion.getText().isEmpty()){
+            labelOperador1.setText(labelOperador1.getText() + "1");
+            // Se comprueba en el ese si la pantalla resultado está´a para una vez que se ha mostrado
+            // el resultado no se pongan más números en la pantalla del operador 2
+        }else if(labelResultado.getText().isEmpty()){
+            labelOperador2.setText(labelOperador2.getText() + "1");
+        }
+        
+    }
+
+    @FXML
+    private void clickBtn2(ActionEvent event) {
+        
+        if(labelOperacion.getText().isEmpty()){
+            labelOperador1.setText(labelOperador1.getText() + "2");
+            // Se comprueba en el ese si la pantalla resultado está´a para una vez que se ha mostrado
+            // el resultado no se pongan más números en la pantalla del operador 2
+        }else if(labelResultado.getText().isEmpty()){
+            labelOperador2.setText(labelOperador2.getText() + "2");
+        }
+        
+    }
+
+    @FXML
+    private void clickBtn3(ActionEvent event) {
+        
+        if(labelOperacion.getText().isEmpty()){
+            labelOperador1.setText(labelOperador1.getText() + "3");
+            // Se comprueba en el ese si la pantalla resultado está´a para una vez que se ha mostrado
+            // el resultado no se pongan más números en la pantalla del operador 2
+        }else if(labelResultado.getText().isEmpty()){
+            labelOperador2.setText(labelOperador2.getText() + "3");
+        }
+        
+    }
+
+    @FXML
+    private void clickBtnAC(ActionEvent event) {
+        resetearPantalla();
+    }
+
+    @FXML
+    private void clickBtnDivision(ActionEvent event) {
+        
+        if(!labelOperador1.getText().isEmpty() && labelOperacion.getText().isEmpty()){
+            labelOperacion.setText(btnDivision.getText());
+        }
+        
+    }
+
+    @FXML
+    private void clickBtnMultiplicacion(ActionEvent event) {
+        
+        if(!labelOperador1.getText().isEmpty() && labelOperacion.getText().isEmpty()){
+            labelOperacion.setText(btnMultiplicacion.getText());
+        }
+        
+    }
+
+    @FXML
+    private void clickBtnSuma(ActionEvent event) {
+        
+        if(!labelOperador1.getText().isEmpty() && labelOperacion.getText().isEmpty()){
+            labelOperacion.setText(btnSuma.getText());
+        }
+        
+    }
+
+    @FXML
+    private void clickBtnResta(ActionEvent event) {
+        
+        if(!labelOperador1.getText().isEmpty() && labelOperacion.getText().isEmpty()){
+            labelOperacion.setText(btnResta.getText());
+        }
+        
+    }
+
+    @FXML
+    private void clickBtnPorcentaje(ActionEvent event) {
+        
+        if(!labelOperador1.getText().isEmpty() && labelOperacion.getText().isEmpty()){
+            labelOperacion.setText(btnPorcentaje.getText());
+        }
+        
+    }
+
+    @FXML
+    private void clickBtnResultado(ActionEvent event) {
+        
+        if (!labelOperador1.getText().isEmpty() && !labelOperador2.getText().isEmpty() && !labelOperacion.getText().isEmpty()) {
+            double operador1 = Double.parseDouble(labelOperador1.getText());
+            double operador2 = Double.parseDouble(labelOperador2.getText());
+            
+            realizarOperacion(operador1, operador2);
+        }
+        
+    }
+    
+    public void resetearPantalla(){
+        labelOperador1.setText("");
+        labelOperador2.setText("");
+        labelOperacion.setText("");
+        labelResultado.setText("");
+    }
+    
+    public void realizarOperacion(double operador1, double operador2){
+        
+        Operaciones operaciones = new Operaciones();
+        
+        String operacion = labelOperacion.getText();
+        
+        if (operacion.equals("%")) {
+            labelResultado.setText(operaciones.porcentaje(operador1, operador2));
+        }
+        else if(operacion.equals("/")){
+            labelResultado.setText(operaciones.dividir(operador1, operador2));
+        }
+        else if(operacion.equals("x")){
+            labelResultado.setText(operaciones.multiplicar(operador1, operador2));
+        }
+        else if(operacion.equals("+")){
+            labelResultado.setText(operaciones.sumar(operador1, operador2));
+        }
+        else if(operacion.equals("-")){
+            labelResultado.setText(operaciones.restar(operador1, operador2));
+        }
+    }
     
 }
--- JavaFX/Calculadora-javaFX/src/modelo/Operaciones.java
+++ JavaFX/Calculadora-javaFX/src/modelo/Operaciones.java
@@ -8,6 +8,42 @@ package modelo;
  *
  * @author jaimedam
  */
-public class Operaciones {
+public class Operaciones {    
+    
+    public String porcentaje(double operador1, double operador2){
+        double resultado = ((operador1 * operador2)/100);
+        
+        return String.valueOf(resultado);
+    }
+    
+    public String dividir(double operador1, double operador2){
+        double resultado;
+        String salida;
+        if (operador2 == 0) {
+            return salida = "Sin definir";
+        }else{
+            resultado = operador1 / operador2;
+            salida = resultado + "";
+            return salida ;
+        }
+    }
+    
+    public String multiplicar(double operador1, double operador2){
+        double resultado = operador1 * operador2;
+        
+        return String.valueOf(resultado);
+    }
+    
+    public String sumar(double operador1, double operador2){
+        double resultado = operador1 + operador2;
+        
+        return String.valueOf(resultado);
+    }
+    
+    public String restar(double operador1, double operador2){
+        double resultado = operador1 - operador2;
+        
+        return String.valueOf(resultado);
+    }
     
 }
