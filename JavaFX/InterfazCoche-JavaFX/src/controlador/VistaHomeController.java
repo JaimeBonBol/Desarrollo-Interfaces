1a18a0b27c99d78208335a894beb71135a5ccf62
JaimeBonBol <jaimebb2002@gmail.com>
2025-10-02 20:28:26 +0200

    Comienzo diseño interfaz pantalla coche

--- JavaFX/InterfazCoche-JavaFX/src/controlador/VistaHomeController.java
+++ JavaFX/InterfazCoche-JavaFX/src/controlador/VistaHomeController.java
@@ -7,13 +7,15 @@ package controlador;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
 /**
@@ -21,10 +23,22 @@ import javafx.stage.Stage;
  *
  * @author jaimedam
  */
-public class Vista2Controller implements Initializable {
+public class VistaHomeController implements Initializable {
 
     @FXML
-    private Button btnVolver;
+    private AnchorPane vistaHome;
+    @FXML
+    private Label labelTime;
+    @FXML
+    private Label labelDate;
+    @FXML
+    private ImageView iconGps;
+    @FXML
+    private ImageView iconMultimedia;
+    @FXML
+    private ImageView iconCarSettings;
+    @FXML
+    private ImageView iconPhone;
 
     /**
      * Initializes the controller class.
@@ -35,20 +49,18 @@ public class Vista2Controller implements Initializable {
     }    
 
     @FXML
-    private void volver(ActionEvent event) throws IOException {
-                // Cargar el FXML de gestión de productos
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("/vista/vista1.fxml"));
+    private void cambiarVistaGps(MouseEvent event) throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/vista/vistaGps.fxml"));
 
         Parent root = loader.load();
         Scene escena = new Scene(root);
 
         // Obtener el Stage actual desde el botón
-        Stage stage = (Stage) btnVolver.getScene().getWindow();
+        Stage stage = (Stage) iconGps.getScene().getWindow();
 
         // Reemplazar la escena actual
         stage.setScene(escena);
-        stage.setTitle("Vista 1");
-
+        stage.setTitle("GPS");
     }
     
 }
